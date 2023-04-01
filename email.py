c95cb646be2f116a146e20013c1a58b64b43bbea
beata098 <bbeata.ch0987@gmail.com>
2023-04-01 10:58:09 +0200

    zadanie 1

--- email.py
+++ email.py
@@ -1,17 +1,44 @@
 import re
 
+#([a-zA-Z]+)\.([a-zA-Z]+)[0-9]*@(\bstudent\b\.)?(wat\.edu\.pl)
 
 class EmailExtractor:
+
     def __init__(self, email):
         self.email = email
+        self.name = None
+        self.surname = None
+        self.isStudent = None
+        self.regex()
+        #self.regex = r'([a-zA-Z]+)\.([a-zA-Z]+)[0-9]*@(\bstudent\b\.)?(wat\.edu\.pl)'
+
+    def regex(self):
+        regex = r'([a-zA-Z]+)\.([a-zA-Z]+)[0-9]*@(\bstudent\b\.)?(wat\.edu\.pl)'
+        results = re.match(regex, self.email)
+        if results:
+            self.name = results.group(1)
+            self.surname = results.group(2)
+            self.isStudent = bool(results.group(3))
+        return self.name, self.surname, self.isStudent
+
 
     def is_student(self) -> bool:
-        x = re.compile("")
-        x.match(self.email)
-        return False
+        #self.regex()
+        return self.isStudent
+
+    def is_male(self) -> bool:
+        lastCharacter = self.name[-1]
+        if lastCharacter == 'a':
+            return False
+        else:
+            return True
 
     def get_surname(self) -> str:
-        return str
+        #self.regex()
+        surname = self.surname.capitalize()
+        return surname
 
     def get_name(self) -> str:
-        return str
+        #self.regex()
+        name = self.name.capitalize()
+        return name
--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 import unittest
+import re
 
 from email import EmailExtractor
 
 
 class EmailExtractorTestCase(unittest.TestCase):
+
+    print("aaa")
+    email = "anna.kowalska@uw.edu.pl"
+
+    match_result = re.match(r'^([a-zA-Z]+)\.([a-zA-Z]+)@.*(?:student)?.*uw\.edu\.pl$', email)
+
+    if match_result:
+        print("Imię:", match_result.group(1))
+        print("Nazwisko:", match_result.group(2))
+        print("Czy to student:", bool("student" in email))
+    else:
+        print("Nie znaleziono dopasowania")
+
+
+
+
+    matches = re.match(r'([a-zA-Z]+)\.([a-zA-Z]+)@.*(?:student)?.*uw\.edu\.pl+$', email)
+    if matches:
+        first_name = matches.group(1)
+        last_name = matches.group(2)
+
+
+
     def setUp(self) -> None:
         self.data = [
             # email, is_student, is_male, name, surname
@@ -12,7 +36,16 @@ class EmailExtractorTestCase(unittest.TestCase):
             ["anna.nowak@student.wat.edu.pl", True, False, "Anna", "Nowak"],
             ["adrianna.abacka01@student.wat.edu.pl", True, False, "Adrianna", "Abacka"],
             ["katarzyna.babacka@wat.edu.pl", False, False, "Katarzyna", "Babacka"],
-            ["anna.kowal@student.wat.edu.pl", True, False, "Anna", "Kowal"]]
+            ["anna.kowal@student.wat.edu.pl", True, False, "Anna", "Kowal"],
+            ["joanna.kowalczyk@student.wat.edu.pl", True, False, "Joanna", "Kowalczyk"],
+            ["justyna.lewandowska@student.wat.edu.pl", True, False, "Justyna", "Lewandowska"],
+            ["jan.zamojski@wat.edu.pl", False, True, "Jan", "Zamojski"],
+            ["tomasz.pisarek@wat.edu.pl", False, True, "Tomasz", "Pisarek"],
+            ["piotr.przybysz@student.wat.edu.pl", True, True, "Piotr", "Przybysz"],
+            ["andrzej.nowakowski@wat.edu.pl", False, True, "Andrzej", "Nowakowski"],
+            ["magdalena.mazur@student.wat.edu.pl", True, False, "Magdalena", "Mazur"],
+            ["katarzyna.bieniek@student.wat.edu.pl", True, False, "Katarzyna", "Bieniek"],
+            ["julia.wysocka@wat.edu.pl", False, False, "Julia", "Wysocka"]]
 
 
     def test_is_student(self):
@@ -32,10 +65,11 @@ class EmailExtractorTestCase(unittest.TestCase):
                 # given
                 email = x[0]
                 name = x[3]
+                is_male = x[2]
                 # then
                 extractor = EmailExtractor(email)
                 # expect
-                self.assertEqual(name, extractor.get_name())
+                self.assertEqual(is_male, extractor.is_male())
 
     def test_get_surname(self):
         for x in self.data:
@@ -58,6 +92,8 @@ class EmailExtractorTestCase(unittest.TestCase):
                 extractor = EmailExtractor(email)
                 # expect
                 self.assertEqual(name, extractor.get_name())
+
+
 
 
 
